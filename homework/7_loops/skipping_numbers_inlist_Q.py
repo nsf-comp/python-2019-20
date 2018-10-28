dd2539e78408385020e098906445b416c5f284b2
pineapplevalley <ayammanuru@gmail.com>
2018-10-27 20:30:35 -0500

    grader for hw2

---
+++ homework/7_loops/skipping_numbers_inlist_Q.py
@@ -0,0 +1,20 @@
+"""
+NSF - CODING - Loops
+Create Date: Oct 17, 2018
+
+Level of Complexity*: 3
+ *Scale:
+    1 - Very Easy
+    2 - Easy
+    3 - Medium
+    4 - Complex
+    5 - Highly Complex
+"""
+
+mylist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+"""
+Write a Python logic which prints only even numbers from the given 'mylist'
+
+"""
+
